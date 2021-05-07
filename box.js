77bd898b8582f5580b0a160c5d59b5786813959d
arjunkejriwal <73331271+arjunkejriwal@users.noreply.github.com>
2021-05-07 15:51:14 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,15 @@
+class Box {
+    constructor(x, y, w, h){
+        var options = {isStatic:true}
+     this.body = Bodies.rectangle(x, y, w, h, options)
+     World.add(world, this.body)
+     this.w = w;
+     this.h = h;
+     this.image = loadImage("dustbingreen.png")
+    }
+   
+    display(){
+    image(this.image, this.body.position.x, this.body.position.y, this.w, this.h)
+     //rect(this.body.position.x, this.body.position.y, this.w, this.h)
+    }
+   }
